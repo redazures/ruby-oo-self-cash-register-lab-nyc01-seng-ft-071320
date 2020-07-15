e5f43a602d1cbf4b045dca4057587690052087be
redazures <jwu_nyc@yahoo.com>
2020-07-14 22:13:30 -0400

    done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
 
+class CashRegister
+    
+    attr_accessor :total, :discount, :items, :count, :price
+    
+    def initialize (discount=nil)
+        @total=0
+        @discount=discount
+        @items=[]
+    end
+
+    def add_item (title, price, count=nil)
+        @items<<title
+        temp=Array.new(count-1,title) if count!=nil
+        @items.concat(temp) if count!=nil
+        @total=@total+(price*count) if count!=nil
+        @total=@total+price if count == nil
+        @count=count
+        @price=price
+    end
+
+    def apply_discount
+        phrase="There is no discount to apply."
+        if discount !=nil
+            @total=@total-200
+            phrase= "After the discount, the total comes to $#{@total}."
+        end
+        phrase
+    end
+
+    def void_last_transaction
+        @total=@total-@price if count ==nil
+        @total=@total-(@price*count) if count != nil
+    end
+
+end
